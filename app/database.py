28b86b4724fb957e6cff490dfc3595c7ce93f708
rommeljo <johnrommel031@gmail.com>
2025-11-13 22:16:00 +0300

    push

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:johnrommel@172.17.0.1:5432/store_db?sslmode=disable"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:johnrommel@172.17.0.1:5432/store_db?sslmode=disable"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
